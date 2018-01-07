db2b939dc78898dbc2e9dedd48f2981e64998c0e
FilippoPedrazziniFP <filippo.pedrazzini94@gmail.com>
2018-01-07 12:19:10 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup, find_packages
+from distutils.core import setup
 
 setup(
   name = 'skopos',
@@ -6,7 +6,7 @@ setup(
   version = '0.1',
   description = 'Deep Reinforcement Learning Library',
   author = 'Skopos-team',
-  packages=[package for package in find_packages() if package.startswith('skopos')],
+  # packages=[package for package in find_packages() if package.startswith('skopos')],
   author_email = 'skopos.library@gmail.com',
   url = 'https://github.com/Skopos-team/Skopos', 
   download_url = 'https://github.com/Skopos-team/Skopos/archive/0.1.tar.gz',
